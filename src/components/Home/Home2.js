805b70f800f3af2f28aa18dc53a64020d3333043
Shaik Shandhani <shaikshandhani91@gmail.com>
2025-11-15 17:16:20 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/image.jpg";
+import myImg from "../../Assets/image.jpeg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
