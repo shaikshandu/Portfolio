f20df7c4853127ab29694bf0a03663f6dd0a1d1a
Shaik Shandhani <shaikshandhani91@gmail.com>
2025-11-15 17:00:46 +0530

    Replace homeLogo image with SVG version

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/photo1.jpg";
+import homeLogo from "../../Assets/homo-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
