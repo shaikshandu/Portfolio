c0b153f72a850aa7ef97a022a2bda00eb7569c37
Shaik Shandhani <shaikshandhani91@gmail.com>
2025-11-18 20:53:03 +0530

    Replace image source with homo-main.svg

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/image.png";
+import myImg from "../../Assets/homo-main.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
