9e2a9a48baf71a48835d031ff0f474d20b9adb94
Shaik Shandhani <shaikshandhani91@gmail.com>
2025-11-19 14:48:27 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,52 +9,58 @@ import { FaLinkedinIn, FaDiscord } from "react-icons/fa";
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by Shaik Sandhani</h3>
         </Col>
+
         <Col md="4" className="footer-copywright">
-          <h3>Made with React.js</h3>
+          <h3>© {year} My Portfolio</h3>
         </Col>
+
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
                 href="https://github.com/shaikshandu"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://discord.com/channels/@me"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaDiscord />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/shaik-sandhani-abab34344/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.instagram.com/shaik_shandu/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
@@ -68,7 +74,3 @@ function Footer() {
 }
 
 export default Footer;
-<footer>
-  © {year} My Portfolio
-</footer>
-
