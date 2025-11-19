8569f9513ca61917dd3a5ef0de54cbdb50c56fd6
Shaik Shandhani <shaikshandhani91@gmail.com>
2025-11-19 14:51:54 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/images.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -20,33 +17,31 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
+
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          
+          {/* Project 1 */}
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
-              description="This project implements real-time object detection using the YOLO. The system uses a webcam to capture live video streams and detect multiple objects such as people, cars, bottles, chairs, and more, based on the COCO dataset"
+              description="This project implements real-time object detection using YOLO. It captures live video and detects objects like people, cars, bottles, chairs, etc. using webcam feed and COCO dataset."
               ghLink="https://github.com/shaikshandu/Real-Time-Object-Detection-With-OpenCV-master"
-              
             />
           </Col>
 
-         
-
-         
-
+          {/* Project 2 */}
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              description="Trained a CNN classifier using the FER-2013 dataset with Keras and TensorFlow. Integrated with OpenCV for live face detection. Achieved 60.1% accuracy in predicting emotions."
               ghLink="https://github.com/shaikshandu/RealFace_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
+
         </Row>
       </Container>
     </Container>
